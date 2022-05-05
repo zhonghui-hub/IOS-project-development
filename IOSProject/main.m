7249ae595e8e9d9941318a1d8a112e70e744ffb6
1144576476@qq.com <1144576476@qq.com>
2022-05-05 11:33:17 +0800

    项目内容打印了你好，你的名字

--- IOSProject/main.m
+++ IOSProject/main.m
@@ -11,6 +11,8 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSString *str = @"你好，我的名字！";
+        NSLog(@"%@", str);
     }
     return 0;
 }
