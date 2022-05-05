effccc03f5d716d5611ebcd64551048e1be06932
1144576476@qq.com <1144576476@qq.com>
2022-05-05 11:22:04 +0800

    Initial Commit

---
+++ IOSProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOSProject
+//
+//  Created by 钟辉 on 2022/5/5.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
